4fd78b2a02701b3bd20a5ac22208e225f6c49ed5
Akash <akashnandhu004@gmail.com>
2025-09-22 21:27:06 +0530

    adding programs

---
+++ sql.py
@@ -0,0 +1,43 @@
+import sqlite3
+connection = sqlite3.connect("akashs.db")
+cursor = connection.cursor()
+
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS student (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(20) NOT NULL,
+        age INTEGER,
+        gender VARCHAR(20),
+        email VARCHAR(30) UNIQUE,
+        phone VARCHAR(30)
+    )
+ """)
+cursor.execute("""
+INSERT INTO student(name,age,gender,email,phone)VALUES
+               ("akhila",45,"male","akhila004@gamil.com","46584479213")
+""")
+
+cursor.execute("SELECT * FROM student")
+rows=cursor.fetchall()
+for row in rows:
+    print(row)
+
+
+try:
+    cursor.execute("""
+        UPDATE student
+        SET email='jebin6884@gmail.com'
+        WHERE name='akhila'
+    """)
+except sqlite3.IntegrityError as e:
+    print("Update failed due to constraint:", e)
+
+
+cursor.execute("""
+DELETE FROM student WHERE name="nandhu" 
+""")
+
+
+connection.commit()
+connection.close()
