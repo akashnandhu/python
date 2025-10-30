0f30a395956a03f7174d4fd5adcf426fdcf34652
Akash <akashnandhu004@gmail.com>
2025-10-30 13:58:11 +0530

    some programs

--- task5.py
+++ task5.py
@@ -1,7 +1,10 @@
-text=input("Enter a string : ")
-reverse=""
-for i in range(len(text) -1, -1, -1 ):
-    reverse += text[i]
-print("Reverse:",reverse)
+# text=input("Enter a string : ")
+# reverse=""
+# for i in range(len(text) -1, -1, -1 ):
+#     reverse += text[i]
+# print("Reverse:",reverse)
+
+
+
 
 
